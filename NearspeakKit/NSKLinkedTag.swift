b3624de67ef3b0f9946ad9c2e4d02ce89a62e371
Patrick <patrick@helmsdeep.at>
2015-02-03 17:23:27 +0100

    NSKLinkedTag supports nscoding

--- NearspeakKit/NSKLinkedTag.swift
+++ NearspeakKit/NSKLinkedTag.swift
@@ -8,8 +8,37 @@
 
 import Foundation
 
-public struct NSKLinkedTag {
-    public var id: NSNumber
-    public var name: String
-    public var identifier: String
+/// NSCoding Keys
+let keyNSKLinkedTagId = "linkedTag_id"
+let keyNSKLinkedTagName = "linkedTag_name"
+let keyNSKLinkedTagIdentifier = "linkedTag_identifier"
+
+public class NSKLinkedTag: NSObject, NSCoding {
+    public var id: NSNumber = 0
+    public var name: String?
+    public var identifier: String?
+    
+    public init(id: NSNumber) {
+        super.init()
+        
+        self.id = id
+    }
+    
+    public init(id: NSNumber, name: String, identifier: String) {
+        self.id = id
+        self.name = name
+        self.identifier = identifier
+    }
+    
+    required public init(coder aDecoder: NSCoder) {
+        self.id = aDecoder.decodeIntegerForKey(keyNSKLinkedTagId)
+        self.name = aDecoder.decodeObjectForKey(keyNSKLinkedTagName) as? String
+        self.identifier = aDecoder.decodeObjectForKey(keyNSKLinkedTagIdentifier) as? String
+    }
+    
+    public func encodeWithCoder(aCoder: NSCoder) {
+        aCoder.encodeInt(self.id.intValue, forKey: keyNSKLinkedTagId)
+        aCoder.encodeObject(self.name, forKey: keyNSKLinkedTagName)
+        aCoder.encodeObject(self.identifier, forKey: keyNSKLinkedTagIdentifier)
+    }
 }
