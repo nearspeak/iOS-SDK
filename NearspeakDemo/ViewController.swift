1d7084773adde04db54702a0573474ea9e06f121
Patrick <patrick@helmsdeep.at>
2015-04-23 09:13:32 +0200

    Added some demo code

--- NearspeakDemo/ViewController.swift
+++ NearspeakDemo/ViewController.swift
@@ -13,6 +13,24 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        var api = NSKApi(devMode: false)
+        
+        api.getAuthToken(username: "demo@appaya.at", password: "dasistnearspeak") { (succeeded, auth_token) -> () in
+            if succeeded {
+                var alertController = UIAlertController(title: "Auth-Token", message: "Your auth-token: \(auth_token)", preferredStyle: UIAlertControllerStyle.Alert)
+                
+                alertController.addAction(UIAlertAction(title: "OK", style: UIAlertActionStyle.Cancel, handler: nil))
+                
+                self.presentViewController(alertController, animated: true, completion: nil)
+            } else {
+                var alertController = UIAlertController(title: "ERROR", message: "Error while requesting the auth_token", preferredStyle: UIAlertControllerStyle.Alert)
+                
+                alertController.addAction(UIAlertAction(title: "OK", style: UIAlertActionStyle.Cancel, handler: nil))
+                
+                self.presentViewController(alertController, animated: true, completion: nil)
+            }
+        }
     }
 
     override func didReceiveMemoryWarning() {
